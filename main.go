a6147ba35f5290ce13c62db110a91cbf039ab8c0
Wouter Beets <beetswouter@gmail.com>
2014-10-24 11:07:11 +0200

    test

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/andlabs/ui"
+)
+
+var window ui.Window
+
+func main() {
+	go ui.Do(func() {
+		name := ui.NewTextField()
+		button := ui.NewButton("Greet")
+		greeting := ui.NewLabel("")
+		stack := ui.NewVerticalStack(
+			ui.NewLabel("Enter your name:"),
+			name,
+			button,
+			greeting)
+		window = ui.NewWindow("Hello", 200, 100, stack)
+		button.OnClicked(func() {
+			greeting.SetText("Hello, " + name.Text() + "!")
+		})
+		window.OnClosing(func() bool {
+			ui.Stop()
+			return true
+		})
+		window.Show()
+	})
+	err := ui.Go()
+	if err != nil {
+		panic(err)
+	}
+}
