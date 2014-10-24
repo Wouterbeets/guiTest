de3cba4899d8175923b503199ea3ef6c79340507
Wouter Beets <beetswouter@gmail.com>
2014-10-24 12:11:56 +0200

    chekcitout

--- main.go
+++ main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/andlabs/ui"
 )
 
 var window ui.Window
 
+type ConfStruct struct {
+	R6_user   string
+	R6_pass   string
+	R6_appkey string
+}
+
+var conf ConfStruct
+
 func main() {
 	go ui.Do(func() {
-		name := ui.NewTextField()
-		button := ui.NewButton("Greet")
-		greeting := ui.NewLabel("")
+		R6_user := ui.NewTextField()
+		R6_pass := ui.NewTextField()
+		R6_appkey := ui.NewTextField()
+		save := ui.NewButton("Save")
+		userLabel := ui.NewLabel("radian6 username")
+		passLabel := ui.NewLabel("password")
+		appKey := ui.NewLabel("appkey")
 		stack := ui.NewVerticalStack(
-			ui.NewLabel("Enter your name:"),
-			name,
-			button,
-			greeting)
-		window = ui.NewWindow("Hello", 200, 100, stack)
-		button.OnClicked(func() {
-			greeting.SetText("Hello, " + name.Text() + "!")
+			userLabel,
+			R6_user,
+			passLabel,
+			R6_pass,
+			appKey,
+			R6_appkey,
+			save,
+		)
+		window = ui.NewWindow("Hello", 800, 400, stack)
+		save.OnClicked(func() {
+			conf.R6_user = R6_user.Text()
+			conf.R6_pass = R6_pass.Text()
+			conf.R6_appkey = R6_appkey.Text()
+			js, err := json.Marshal(&conf)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(js))
 		})
 		window.OnClosing(func() bool {
 			ui.Stop()
